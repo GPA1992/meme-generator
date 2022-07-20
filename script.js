fbe8dae9c3c1c9724a707fba8b23620c46ec5891
Gabriel <gabrielproencaaraujo@gmail.com>
2022-07-20 14:00:03 -0300

    Projeto Finalizado

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const inputText = document.querySelector('#text-input');
+const memeText = document.querySelector('#meme-text');
+const imgMeme = document.querySelector('#meme-image');
+const memeInput = document.querySelector('#meme-insert');
+
+memeInput.addEventListener('change', (e) => {
+    const reader = new FileReader();
+    console.log(reader);
+    reader.onload = () => {
+     imgMeme.src = reader.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);
+    document.querySelector('#meme-text').innerHTML = inputText.value;
+});
