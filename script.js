101e9cf0f0fe75ef4bc04c2f2576ab7abd83d989
Gabriel <gabrielproencaaraujo@gmail.com>
2022-07-20 14:32:37 -0300

    Erros corrigidos

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-
 const inputText = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
 const imgMeme = document.querySelector('#meme-image');
 const memeInput = document.querySelector('#meme-insert');
 
+inputText.addEventListener('keyup', () => {
+  memeText.innerHTML = inputText.value;
+});
+
 memeInput.addEventListener('change', (e) => {
-    const reader = new FileReader();
-    console.log(reader);
-    reader.onload = () => {
-     imgMeme.src = reader.result;
-    }
-    reader.readAsDataURL(e.target.files[0]);
-    document.querySelector('#meme-text').innerHTML = inputText.value;
+  const reader = new FileReader();
+  console.log(reader);
+  reader.onload = () => {
+    imgMeme.src = reader.result;
+  };
+  reader.readAsDataURL(e.target.files[0]);
+  document.querySelector('#meme-text').innerHTML = inputText.value;
 });
